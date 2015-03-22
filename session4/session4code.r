b8e8f35c2ddda9d6981630b232e5339c598b685e
Mandy Vogel <mandy.vogel@googlemail.com>
2015-03-22 17:31:33 +0100

    begin to add simulation exercises

--- session4/session4code.r
+++ session4/session4code.r
@@ -50,6 +50,57 @@ ztest <- function(x,x.sd,mu=0){
 set.seed(1)
 ztest(rnorm(100),x.sd = 1)
 
+######################################################################
+####################### Simulation exerc #############################
+######################################################################
+
+## Now sample 100 values from a Normal distribution with mean 10
+## and standard deviation 2 and use a z-test to compare it against
+## the population mean 10. What is the p-value?
+
+ztest(rnorm(100,mean=10,sd=2),x.sd=2,mu=10)["pval"]
+ztest(rnorm(100,mean=10,sd=2),x.sd=2,mu=10)["diff"]
+ztest(rnorm(100,mean=10,sd=2),x.sd=2,mu=10)[c("pval","diff")]
+
+## Now do the sampling and the testing 1000 times, what would be the
+## number of statistically significant results? Use replicate()
+## (which is a wrapper of tapply()) or a for() loop! Record at
+## least the p-values and estimated differences!
+
+### replicate()
+res <- replicate(1000, ztest(rnorm(100,mean=10,sd=2),x.sd=2,mu=10))
+res <- as.data.frame(t(res))
+
+
+### replicate(,simplify=F) 
+res <- replicate(1000, ztest(rnorm(100,mean=10,sd=2),x.sd=2,mu=10),simplify = F)
+res <- as.data.frame(Reduce(rbind,res))
+
+
+## for() loop
+res <- matrix(numeric(2000),ncol=2)
+for(i in seq.int(1000)){
+    res[i,] <- ztest(rnorm(100,mean=10,sd=2),x.sd=2,mu=10)[c("pval","diff")] }
+res <- as.data.frame(res)
+names(res) <- c("pval","diff")
+
+## Use table() to count the p-vals below 0.05.
+table(res$pval < 0.05)
+
+## What is the smallest absolute difference with a p-value below 0.05?
+tapply(abs(res$diff),res$pval < 0.05,summary)
+
+
+## Repeat the last exercise, only change the sample size to 1000 in each of the 1000 samples! How many p-value below 0.05? What is now the smallest absolute difference with a p-value below 0.05?
+res <- replicate(1000, ztest(rnorm(1000,mean=10,sd=2),x.sd=2,mu=10))
+res <- as.data.frame(t(res))
+
+## Use table() to count the p-vals below 0.05.
+table(res$pval < 0.05)
+
+## What is the smallest absolute difference with a p-value below 0.05?
+tapply(abs(res$diff),res$pval < 0.05,summary)
+
 
 ######################################################################
 ####################### T-Test #######################################
